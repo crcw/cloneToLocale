5c79acdb9a288b7d866eed503b1b211004a5cd3c
crcw <crcw@vip.qq.com>
2015-10-29 22:26:19 +0800

    Update Client.java
    update System.out.println("github server update======");

--- src/com/github/test/Client.java
+++ src/com/github/test/Client.java
@@ -7,5 +7,6 @@ public class Client {
 		System.out.println("I am here !  locale");
 		System.out.println("client say:这是我本地更新， 收到请回");
 		System.out.println("github server say:收到了收到");
+		System.out.println("github server update======");
 	}
 }
